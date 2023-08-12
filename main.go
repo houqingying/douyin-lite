9e08d92e7173f920ca9b22b29b2c4748af135e19
Lancelot <1984737645@qq.com>
2023-08-13 02:08:28 +0800

    feat: configuration with viper
    Signed-off-by: Lancelot <1984737645@qq.com>

--- main.go
+++ main.go
@@ -1,4 +1,12 @@
 package main
 
+import (
+	"github.com/houqingying/douyin-lite/pkg/config"
+	"k8s.io/klog"
+)
+
 func main() {
+	if err := config.Setup(); err != nil {
+		klog.Fatalf("config.Setup() error: %s", err)
+	}
 }
---
+++ pkg/config/database.go
@@ -0,0 +1,11 @@
+package config
+
+type DatabaseConfiguration struct {
+	Driver   string
+	Dbname   string
+	Username string
+	Password string
+	Host     string
+	Port     string
+	LogMode  bool
+}
