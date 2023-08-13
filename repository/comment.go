b29a3b178898f2d8059fb5ad044aeb603736524e
Lancelot <1984737645@qq.com>
2023-08-13 17:29:50 +0800

    feat: use commentDao to operate comments from database
    Signed-off-by: Lancelot <1984737645@qq.com>

--- repository/comment.go
+++ repository/comment.go
@@ -8,33 +8,100 @@ import (
 
 type Comment struct {
 	gorm.Model
-	VideoId   string `json:"video_id"`
-	UserId    string `json:"user_id"`
-	CommentId string `json:"comment_id"`
+	VideoId string `json:"video_id"`
+	UserId  string `json:"user_id"`
+	Comment string `json:"comment"`
 }
 
 type CommentDao struct {
 }
 
-var commentDao *UserDao
+var commentDao *CommentDao
 var CommentOnce sync.Once
 
-func NewCommentDaoInstance() *UserDao {
-	UserOnce.Do(func() {
-		userDao = &UserDao{}
+func NewCommentDaoInstance() *CommentDao {
+	CommentOnce.Do(func() {
+		commentDao = &CommentDao{}
 	})
-	return userDao
+	return commentDao
 }
 
-func (*UserDao) CreateComment(name string, followingCnt uint, followerCnt uint) error {
-	newUser := User{
-		Name:           name,
-		FollowingCount: followingCnt,
-		FollowerCount:  followerCnt,
+// CreateComment creates a comment
+func (*CommentDao) CreateComment(videoId string, userId string, comment string) error {
+	newComment := Comment{
+		VideoId: videoId,
+		UserId:  userId,
+		Comment: comment,
 	}
-	err := db.Create(&newUser).Error
+	err := db.Create(&newComment).Error
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+// DeleteComment deletes a comment
+func (*CommentDao) DeleteComment(id uint) error {
+	err := db.Where("id = ?", id).Delete(&Comment{}).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// DeleteCommentByVideoId deletes a comment by video id
+func (*CommentDao) DeleteCommentByVideoId(videoId string) error {
+	err := db.Where("video_id = ?", videoId).Delete(&Comment{}).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// DeleteCommentByUserId deletes a comment by user id
+func (*CommentDao) DeleteCommentByUserId(userId string) error {
+	err := db.Where("user_id = ?", userId).Delete(&Comment{}).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// UpdateComment updates a comment
+func (*CommentDao) UpdateComment(id uint, comment string) error {
+	err := db.Model(&Comment{}).Where("id = ?", id).Update("comment", comment).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// QueryCommentById gets a comment by id
+func (*CommentDao) QueryCommentById(id uint) (*Comment, error) {
+	var comment Comment
+	err := db.Where("id = ?", id).First(&comment).Error
+	if err != nil {
+		return nil, err
+	}
+	return &comment, nil
+}
+
+// QueryCommentsByVideoId gets comments by video id
+func (*CommentDao) QueryCommentsByVideoId(videoId string) ([]Comment, error) {
+	var comments []Comment
+	err := db.Where("video_id = ?", videoId).Find(&comments).Error
+	if err != nil {
+		return nil, err
+	}
+	return comments, nil
+}
+
+// QueryCommentsByUserId gets comments by user id
+func (*CommentDao) QueryCommentsByUserId(userId string) ([]Comment, error) {
+	var comments []Comment
+	err := db.Where("user_id = ?", userId).Find(&comments).Error
+	if err != nil {
+		return nil, err
+	}
+	return comments, nil
 }
---
+++ repository/comment_test.go
@@ -0,0 +1,253 @@
+package repository
+
+import (
+	"testing"
+
+	"github.com/houqingying/douyin-lite/pkg/config"
+	"k8s.io/klog"
+)
+
+// init
+func init() {
+	// 1. Initialize configuration
+	if err := config.Setup(); err != nil {
+		klog.Fatalf("config.Setup() error: %s", err)
+	}
+	// 2. Initialize database
+	if err := Setup(&config.Config.Database); err != nil {
+		klog.Fatalf("repository.Setup() error: %s", err)
+	}
+}
+
+func TestCommentDao_CreateComment(t *testing.T) {
+
+	type args struct {
+		videoId string
+		userId  string
+		comment string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "create comment",
+			args: args{
+				videoId: "1",
+				userId:  "1",
+				comment: "test",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			co := &CommentDao{}
+			if err := co.CreateComment(tt.args.videoId, tt.args.userId, tt.args.comment); (err != nil) != tt.wantErr {
+				t.Errorf("CreateComment() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestCommentDao_DeleteComment(t *testing.T) {
+	type args struct {
+		id uint
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "delete comment",
+			args: args{
+				id: 1,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			co := &CommentDao{}
+			if err := co.DeleteComment(tt.args.id); (err != nil) != tt.wantErr {
+				t.Errorf("DeleteComment() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestCommentDao_DeleteCommentByVideoId(t *testing.T) {
+	type args struct {
+		videoId string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "delete comment by video id",
+			args: args{
+				videoId: "0",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			co := &CommentDao{}
+			if err := co.DeleteCommentByVideoId(tt.args.videoId); (err != nil) != tt.wantErr {
+				t.Errorf("DeleteCommentByVideoId() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestCommentDao_QueryCommentById(t *testing.T) {
+	type args struct {
+		id uint
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *Comment
+		wantErr bool
+	}{
+		{
+			name: "query comment by id",
+			args: args{
+				id: 2,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			co := &CommentDao{}
+			got, err := co.QueryCommentById(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("QueryCommentById() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if (got == nil) != tt.wantErr {
+				t.Errorf("QueryCommentById() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCommentDao_QueryCommentsByUserId(t *testing.T) {
+	type args struct {
+		userId string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []Comment
+		wantErr bool
+	}{
+		{
+			name: "query comments by user id",
+			args: args{
+				userId: "1",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			co := &CommentDao{}
+			got, err := co.QueryCommentsByUserId(tt.args.userId)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("QueryCommentsByUserId() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if (got == nil) != tt.wantErr {
+				t.Errorf("QueryCommentsByUserId() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCommentDao_QueryCommentsByVideoId(t *testing.T) {
+	type args struct {
+		videoId string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []Comment
+		wantErr bool
+	}{
+		{
+			name: "query comments by video id",
+			args: args{
+				videoId: "1",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			co := &CommentDao{}
+			got, err := co.QueryCommentsByVideoId(tt.args.videoId)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("QueryCommentsByVideoId() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if (got == nil) != tt.wantErr {
+				t.Errorf("QueryCommentsByVideoId() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCommentDao_UpdateComment(t *testing.T) {
+	type args struct {
+		id      uint
+		comment string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "update comment",
+			args: args{
+				id:      2,
+				comment: "update comment",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			co := &CommentDao{}
+			if err := co.UpdateComment(tt.args.id, tt.args.comment); (err != nil) != tt.wantErr {
+				t.Errorf("UpdateComment() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestCommentDao_DeleteCommentByUserId(t *testing.T) {
+	type args struct {
+		userId string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "delete comment by user id",
+			args: args{
+				userId: "0",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			co := &CommentDao{}
+			if err := co.DeleteCommentByUserId(tt.args.userId); (err != nil) != tt.wantErr {
+				t.Errorf("DeleteCommentByUserId() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
