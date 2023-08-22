a04d9c270b69e8b85ce02f35fcd07433d5f3706f
zl264 <2644765104@qq.com>
2023-08-22 22:40:56 +0800

    注释entity中的test

--- internal/entity/follower_test.go
+++ internal/entity/follower_test.go
@@ -1,34 +1,34 @@
 package entity
 
-import (
-	"douyin-lite/configs"
-	"fmt"
-	"testing"
-)
+// import (
+// 	"douyin-lite/configs"
+// 	"fmt"
+// 	"testing"
+// )
 
-func TestFollowingDao_QueryFollowerListByHostId(t *testing.T) {
-	configs.Init()
-	userList, err := NewFollowingDaoInstance().QueryFollowerListByHostId(1)
-	if err != nil {
-		panic(err)
-	}
-	for _, user := range userList {
-		fmt.Println(user.Name, " ", user.ID)
-	}
-}
+// func TestFollowingDao_QueryFollowerListByHostId(t *testing.T) {
+// 	configs.Init()
+// 	userList, err := NewFollowingDaoInstance().QueryFollowerListByHostId(1)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	for _, user := range userList {
+// 		fmt.Println(user.Name, " ", user.ID)
+// 	}
+// }
 
-func TestFollowingDao_IncFollowerCnt(t *testing.T) {
-	configs.Init()
-	err := NewFollowingDaoInstance().IncFollowerCnt(4)
-	if err != nil {
-		panic(err)
-	}
-}
+// func TestFollowingDao_IncFollowerCnt(t *testing.T) {
+// 	configs.Init()
+// 	err := NewFollowingDaoInstance().IncFollowerCnt(4)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
 
-func TestFollowingDao_DecFollowerCnt(t *testing.T) {
-	configs.Init()
-	err := NewFollowingDaoInstance().DecFollowerCnt(1)
-	if err != nil {
-		panic(err)
-	}
-}
+// func TestFollowingDao_DecFollowerCnt(t *testing.T) {
+// 	configs.Init()
+// 	err := NewFollowingDaoInstance().DecFollowerCnt(1)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
