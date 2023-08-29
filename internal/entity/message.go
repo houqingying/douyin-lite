b4378812d409e7e61391a65c0bd28b54dece5e21
HouQingying <906410752@qq.com>
2023-08-29 20:12:49 +0800

    change the ID of Message

--- internal/entity/message.go
+++ internal/entity/message.go
@@ -9,9 +9,10 @@ import (
 
 // Message 用户消息对象
 type Message struct {
-	gorm.Model        // 自动生成Unsigned BIGINT作为ID
-	FromUserId uint   `gorm:"column:from_user_id;"`               // 接收方ID，BIGINT
-	ToUserId   uint   `gorm:"column:to_user_id;"`                 // 发送方ID，BIGINT
+	gorm.Model
+	ID         int64  `json:"id" gorm:"id,omitempty"`             // 不自动生成Unsigned BIGINT作为ID，使用snowflake
+	FromUserId int64  `gorm:"column:from_user_id;"`               // 接收方ID，BIGINT
+	ToUserId   int64  `gorm:"column:to_user_id;"`                 // 发送方ID，BIGINT
 	Content    string `gorm:"column:content;type:varchar(1024);"` // 消息内容，VARCHAR(1024)
 }
 
@@ -36,11 +37,11 @@ func GetMessageDaoInstance() *MessageDao {
 
 // CreateMessage 定义MessageDao类型的发送消息方法，将接收到的消息保存到MySQL数据库
 // @auth	hqy			2023/08/17
-// @param	fromUserId	uint	发送方用户Id
-// @param	toUserId	uint	接收方用户Id
+// @param	fromUserId	int64	发送方用户Id
+// @param	toUserId	int64	接收方用户Id
 // @param	content		string	消息内容
 // @return	err			error	当执行出现错误时返回error，否则返回nil
-func (*MessageDao) CreateMessage(fromUserId uint, toUserId uint, content string) error {
+func (*MessageDao) CreateMessage(fromUserId int64, toUserId int64, content string) error {
 	message := Message{
 		FromUserId: fromUserId,
 		ToUserId:   toUserId,
@@ -52,16 +53,16 @@ func (*MessageDao) CreateMessage(fromUserId uint, toUserId uint, content string)
 
 // QueryMessage 查询数据库中fromUserId和toUserId间的所有聊天记录
 // @auth	hqy				2023/08/17
-// @param	fromUserId		uint		发送方用户Id
-// @param	toUserId		uint		接收方用户Id
+// @param	fromUserId		int64		发送方用户Id
+// @param	toUserId		int64		接收方用户Id
 // @return	messageList 	[]*Message	消息记录列表
 // @return	err				error		当执行出现错误时返回error，否则返回nil
-func (*MessageDao) QueryMessage(fromUserId uint, toUserId uint) ([]*Message, error) {
+func (*MessageDao) QueryMessage(fromUserId int64, toUserId int64) ([]*Message, error) {
 	var messageList []*Message
 
 	// 查询表中发送方和接收方参数均在 {fromUserId, toUserId}中的记录
 	err := storage.DB.Where("from_user_id in ? AND to_user_id in ?",
-		[]uint{fromUserId, toUserId}, []uint{fromUserId, toUserId}).Find(&messageList).Error
+		[]int64{fromUserId, toUserId}, []int64{fromUserId, toUserId}).Find(&messageList).Error
 
 	if err != nil {
 		return nil, err
@@ -71,8 +72,8 @@ func (*MessageDao) QueryMessage(fromUserId uint, toUserId uint) ([]*Message, err
 }
 
 // QueryLastMessage QueryMessageByDate 查询数据库中fromUserId和toUserId间的最后一条聊天记录
-// @param 	fromUserId		uint		发送方用户Id
-// @param 	toUserId		uint		接收方用户Id
+// @param 	fromUserId		int64		发送方用户Id
+// @param 	toUserId		int64		接收方用户Id
 // @return message *Message	消息记录
 // @return err		error					当执行出现错误时返回error，否则返回nil
 func (*MessageDao) QueryLastMessage(fromUserId int64, toUserId int64) (*Message, error) {
