56042fe4afbdca4947f4e8044c7eff4c462ff088
Yoke Cheng <18322535679@163.com>
2023-08-29 11:02:07 +0800

    [feat]实现上传文件,获取文件列表功能

--- internal/api/publish.go
+++ internal/api/publish.go
@@ -2,19 +2,20 @@ package api
 
 import (
 	"douyin-lite/configs"
+	"douyin-lite/internal/entity"
 	fastDFS "douyin-lite/pkg/fastdfs"
-	"douyin-lite/pkg/ffmpeg"
 	"douyin-lite/pkg/file"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 type VideoListResponse struct {
 	configs.Response
-	VideoList []configs.Video `json:"video_list"`
+	VideoList []*entity.Video `json:"video_list"`
 }
 
 // Publish 是一个处理文件上传和发布的处理函数。
@@ -57,13 +58,13 @@ func Publish(c *gin.Context) {
 
 	//在服务器上执行ffmpeg 从视频流中获取第一帧截图，并上传图片服务器，保存图片链接
 	//向队列中添加消息
-	if err := ffmpeg.ReadFrameAsJpeg(filePath, imagePath, configs.FrameNum); err != nil {
+	/*if err := ffmpeg.ReadFrameAsJpeg(filePath, imagePath, configs.FrameNum); err != nil {
 		c.JSON(http.StatusOK, configs.Response{
 			StatusCode: http.StatusInternalServerError,
 			StatusMsg:  fmt.Sprintf("%s,错误信息:%s", configs.ErrReadFrameFailure, err.Error()),
 		})
 		return
-	}
+	}*/
 
 	// 上传到dfs
 	obj, err := fastDFS.FDClient.UploadGoFastDFS(filePath)
@@ -75,25 +76,25 @@ func Publish(c *gin.Context) {
 		return
 	}
 
-	/*
-		//保存视频在数据库中
-		video := entity.Video{
-			AuthorId:      userId,
-			PlayUrl:       "http://" + config.Config.VideoServer.Addr2 + "/videos/" + filename,
-			CoverUrl:      "http://" + config.Config.VideoServer.Addr2 + "/photos/" + coverName,
-			FavoriteCount: 0,
-			CommentCount:  0,
-			Title:         replaceTitle,
-			Author:        entity.User{},
-		}
-
-		if err = video.SaveVideo(); err != nil {
-			c.JSON(http.StatusOK, configs.Response{
-				StatusCode: http.StatusInternalServerError,
-				StatusMsg:  configs.ErrDatabaseInsertFailed,
-			})
-			return
-		}*/
+	// 保存到数据库
+	title := c.PostForm("title")
+	userId := c.GetInt64("user_id")
+	//保存视频在数据库中
+	var video = entity.Video{
+		AuthorId:      uint(userId),
+		PlayUrl:       obj["url"].(string),
+		CoverUrl:      imagePath,
+		FavoriteCount: 0,
+		CommentCount:  0,
+		Title:         title,
+	}
+	if err = video.SaveVideo(); err != nil {
+		c.JSON(http.StatusOK, configs.Response{
+			StatusCode: http.StatusInternalServerError,
+			StatusMsg:  configs.ErrDatabaseInsertFailed,
+		})
+		return
+	}
 
 	// 删除本地临时文件
 	if err = os.Remove(filePath); err != nil {
@@ -105,60 +106,34 @@ func Publish(c *gin.Context) {
 	}
 
 	// 返回上传结果
-	c.JSON(http.StatusOK, obj)
+	c.JSON(http.StatusOK, configs.Response{
+		StatusCode: configs.StatusSuccess,
+		StatusMsg:  configs.SuccessMessage,
+	})
 }
 
 // PublishList all users have same publish video list
 func PublishList(c *gin.Context) {
-	c.JSON(http.StatusOK, VideoListResponse{
-		Response: configs.Response{
-			StatusCode: 0,
-		},
-		VideoList: DemoVideos,
-	})
-}
-
-var DemoVideos = []configs.Video{
-	{
-		Id:            1,
-		Author:        configs.User{},
-		PlayUrl:       "https://www.w3schools.com/html/movie.mp4",
-		CoverUrl:      "https://cdn.pixabay.com/photo/2016/03/27/18/10/bear-1283347_1280.jpg",
-		FavoriteCount: 0,
-		CommentCount:  0,
-		IsFavorite:    false,
-	},
-}
-
-// PublishList all users have same publish video list
-/*func PublishList(c *gin.Context) {
 	//获取用户id
 	userId, err := strconv.ParseInt(c.Query("user_id"), 10, 64)
+	var video = entity.Video{}
+	videos, err := video.GetVideoList(userId)
 	if err != nil {
 		c.JSON(http.StatusOK, VideoListResponse{
 			Response: configs.Response{
-				StatusCode: 1,
-				StatusMsg:  "类型转换错误",
+				StatusCode: http.StatusInternalServerError,
+				StatusMsg:  configs.ErrDatabaseQueryFailed,
 			},
 			VideoList: nil,
 		})
-	}
-	publishList, err := GetVideoService().PublishList(userId)
-	if err != nil {
-		c.JSON(http.StatusOK, VideoListResponse{
-			Response: configs.Response{
-				StatusCode: 1,
-				StatusMsg:  "数据库异常",
-			},
-			VideoList: nil,
-		})
+		return
 	}
 	c.JSON(http.StatusOK, VideoListResponse{
 		Response: configs.Response{
-			StatusCode: 0,
-			StatusMsg:  "查询成功",
+			StatusCode: configs.StatusSuccess,
+			StatusMsg:  configs.SuccessMessage,
 		},
-		VideoList: publishList,
+		VideoList: videos,
 	})
+	return
 }
-*/
